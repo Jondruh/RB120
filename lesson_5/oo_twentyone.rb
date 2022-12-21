88463b2dac199b66184cedbefb9f00319b626b7d
Jonathan Hurd <hurd.jonathan@gmail.com>
2022-12-21 09:04:46 -0600

    Refactoring variable names/locations and input validation

--- lesson_5/oo_twentyone.rb
+++ lesson_5/oo_twentyone.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Player
   attr_accessor :hand, :score, :round_score
   attr_reader :name
@@ -38,6 +39,11 @@ end
 class Deck
   attr_accessor :cards
 
+  SUITS = [{ spades: "\u2660" },
+           { hearts: "\u2665" },
+           { diamonds: "\u2666" },
+           { clubs: "\u2663" }]
+
   def initialize
     @cards = []
     build_deck
@@ -50,7 +56,7 @@ class Deck
   private
 
   def build_deck
-    TwentyOne::SUITS.each do |suit|
+    SUITS.each do |suit|
       (2..10).each { |num| cards << Card.new(suit.values[0], num.to_s) }
       %w(J Q K A).each { |face| cards << Card.new(suit.values[0], face) }
     end
@@ -104,10 +110,6 @@ end
 class TwentyOne
   BUST = 21
   DEALER_CUTOFF = 17
-  SUITS = [{ spades: "\u2660" },
-           { hearts: "\u2665" },
-           { diamonds: "\u2666" },
-           { clubs: "\u2663" }]
 
   attr_reader :human, :dealer, :deck, :round_winner, :busted_player
 
@@ -117,7 +119,7 @@ class TwentyOne
       setup_round
       human_turn
       dealer_turn if !human.busted?
-      display_round_results
+      wrap_up_round
 
       break if game_over?
       player_ready_prompt
@@ -148,11 +150,11 @@ class TwentyOne
       input = hit_or_stay
 
       case input
-      when 'h' then
+      when 'h', 'hit' then
         deal_to(human)
         display_table
         break if human.busted?
-      when 's' then break
+      when 's', 'stay' then break
       end
     end
   end
@@ -163,7 +165,7 @@ class TwentyOne
     end
   end
 
-  def display_round_results
+  def wrap_up_round
     update_round_winner
     update_round_scores
     display_table(show_all_cards: true)
@@ -234,11 +236,11 @@ class TwentyOne
 
   def create_players
     name = nil
+    puts "Please enter a name:"
     loop do
-      puts "Please enter a name"
       name = gets.chomp
-      break if !name.empty?
-      puts "Please enter at least one character"
+      break if name.match?(/\S/)
+      puts "Please enter at least one non-whitespace character"
     end
 
     @human = Player.new(name)
@@ -259,15 +261,15 @@ class TwentyOne
     input = nil
     loop do
       input = gets.chomp.downcase
-      break if %w(h s).include?(input)
-      puts "Sorry, please enter 'h' or 's'"
+      break if %w(hit stay h s).include?(input)
+      puts "Sorry, please enter 'h','hit', 'stay' or 's'"
     end
     input
   end
 
   def show_dealer_title(show_score: false)
     score_display = show_score ? dealer.score : "???"
-    puts "Dealer's hand // Dealer's Score: #{score_display}"
+    puts "Dealer's hand // Dealer's hand score: #{score_display}"
   end
 
   def display_table(show_all_cards: false)
@@ -276,7 +278,7 @@ class TwentyOne
     show_dealer_title(show_score: show_all_cards)
     dealer.display_hand(face_up: show_all_cards)
 
-    puts "Your hand. // Your Score #{human.score}"
+    puts "Your hand. // Your hand score #{human.score}"
     human.display_hand
   end
 
